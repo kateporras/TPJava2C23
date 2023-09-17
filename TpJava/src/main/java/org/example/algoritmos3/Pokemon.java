4bc1f5eaff60a7a1058dfeef68e0ec7365b6ff9b
kateporras <porraskate20@gmail.com>
2023-09-17 10:48:17 -0300

    ingresandoClasePokemon

---
+++ TpJava/src/main/java/org/example/algoritmos3/Pokemon.java
@@ -0,0 +1,19 @@
+package org.example.algoritmos3;
+
+public class Pokemon {
+    private String nombre;
+    private int nivel;
+    private Tipos tipo;
+
+    private String historia;
+    private int vidaMaxima=100;
+    private int velocidad;
+    private int defensa;
+    private int ataque;
+
+
+
+
+    
+
+}
